331ee717b2002507935d1de27894c46a3ab48222
Marcus Orochena <marcus.orochena@gmail.com>
2014-08-13 14:57:23 -0400

    fixed usernames for comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140813183631) do
+ActiveRecord::Schema.define(version: 20140813184350) do
 
   create_table "commontator_comments", force: true do |t|
     t.string   "creator_type"
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 20140813183631) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
