4fc86ed579fe6878a8eca6d4cb918cc184d4bce1
Marcus Orochena <marcus.orochena@gmail.com>
2014-08-14 15:30:59 -0400

    added admin auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_admin!, if: :no_admin?
 
   protect_from_forgery with: :exception
 
@@ -8,4 +9,14 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :username
   end
+
+  def no_admin?
+    if Admin.count < 1
+      return true
+    else 
+      return false
+    end
+  end
+
+
 end
