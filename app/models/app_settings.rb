3f01208c320d47324c5674d7d951b36624a3b5bd
Marcus Orochena <marcus.orochena@gmail.com>
2014-08-18 14:02:55 -0400

    registration page changes

--- app/models/app_settings.rb
+++ app/models/app_settings.rb
@@ -1,6 +1,7 @@
 class AppSettings < RailsSettings::CachedSettings
   defaults[:app_name] = "App Name"
   defaults[:app_icon] = "http://placehold.it/512x512"
+  defaults[:app_header_image] = "http://placehold.it/750x200"
   defaults[:about_text] = "This is a pretty cool app and doesn't afraid of anything."
   defaults[:contact_email] = "email@company.com"
   
